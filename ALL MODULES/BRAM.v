d9c5b722da847e1fe17e726198dc3115d2990b55
Dhruv Goel <143334336+dhruv01032005@users.noreply.github.com>
2025-04-19 14:31:23 +0530

    Update BRAM.v

--- ALL MODULES/BRAM.v
+++ ALL MODULES/BRAM.v
@@ -5,5 +5,5 @@ input clk, en, wen, ren;
 input [14:0] addr;
 input [7:0] din;
 output [7:0] dout;
-    BRAM_ALL inst1(.clka(clk),.ena((en&ren)|(en&wen)),.wea(wen),.addra(addr),.dina(din),.douta(dout));
+    blk_mem_gen_0 inst1(.clka(clk),.ena((en&ren)|(en&wen)),.wea(wen),.addra(addr),.dina(din),.douta(dout));
 endmodule
